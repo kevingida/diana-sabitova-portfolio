e7867365a7aac3cd22de871824875de0c3b1531e
kevingida <kevingida@gmail.com>
2025-01-10 11:31:15 +0100

    add scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import Bubble1 from "@/component/Bubble1";
-import Bubble2 from "@/component/Bubble2";
+import ArrowDownRight from "@/component/svg/ArrowDownRight";
+import Bubble1 from "@/component/svg/Bubble1";
+import Bubble2 from "@/component/svg/Bubble2";
 import localFont from "next/font/local";
 const bicubik = localFont({ src: "../font/Bicubik.otf" });
 const clashDisplayMedium = localFont({
@@ -28,7 +29,7 @@ export default function Home() {
         height={1813}
         className="absolute top-[1500px] -right-80"
       />
-      <section className=" w-full h-[1093px] px-[120px] pt-[269px] z-50 grid grid-cols-[490px_auto] grid-rows-[170px_170px_170px] gap-x-6 leading-none uppercase">
+      <section className=" w-full px-[120px] pt-[269px] z-50 grid grid-cols-[490px_auto] grid-rows-[170px_170px_170px] gap-x-6 leading-none uppercase">
         <h1
           className={`${clashDisplayMedium.className} text-[170px] z-10 col-span-2`}
         >
@@ -51,8 +52,45 @@ export default function Home() {
           approach, engaging visuals & world-class quality.
         </p>
       </section>
-      <section>
-        <p>scroll to explore</p>
+      <section className=" w-full px-[120px] pt-[128px]">
+        <div className=" flex flex-row justify-start items-center gap-8">
+          <p className={`${dmSansRegular.className} text-[24px] uppercase`}>
+            scroll to explore
+          </p>
+          <ArrowDownRight height={36} />
+        </div>
+        <div className="pt-[80px] pb-[164px] flex flex-nowrap flex-row gap-12 overflow-x-auto snap-x snap-mandatory">
+          <div
+            style={{ backgroundImage: "url('/Union.svg')" }}
+            className=" w-[400px] h-[500px] flex-none snap-always snap-center z-50 px-[40px] py-[80px]"
+          >
+            <img
+              src="/dianaPhoto.png"
+              alt="Diana Photo"
+              width={125}
+              height={125}
+            />
+            <p>Hello, I’m Diana Sabitova</p>
+            <div className="flex-grow border-t border-gray-400"></div>
+            <p>[ UX/UI Designer ]</p>
+          </div>
+          <div
+            style={{ backgroundImage: "url('/Union.svg')" }}
+            className=" min-w-[400px] h-[500px] flex-none snap-always snap-center z-50"
+          ></div>
+          <div
+            style={{ backgroundImage: "url('/Union.svg')" }}
+            className=" min-w-[400px] h-[500px] flex-none snap-always snap-center z-50"
+          ></div>
+          <div
+            style={{ backgroundImage: "url('/Union.svg')" }}
+            className=" min-w-[400px] h-[500px] flex-none snap-always snap-center z-50"
+          ></div>
+          <div
+            style={{ backgroundImage: "url('/Union.svg')" }}
+            className=" min-w-[400px] h-[500px] flex-none snap-always snap-center z-50"
+          ></div>
+        </div>
       </section>
     </div>
   );
---
+++ component/svg/ArrowDownRight.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+interface ArrowDownRightProps extends React.SVGProps<SVGSVGElement> {}
+
+const ArrowDownRight = (props: ArrowDownRightProps) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M19 17.5005L5 3.50049"
+        stroke="black"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19 7.2305V17.5005H8.73"
+        stroke="black"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.5 22.0005H3.5"
+        stroke="black"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowDownRight;

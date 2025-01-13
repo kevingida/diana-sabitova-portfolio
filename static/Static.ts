033670e4c1783ee1ef3ca7b35ff37b7edbb2de9d
kevingida <kevingida@gmail.com>
2025-01-13 12:24:06 +0100

    add static

---
+++ static/Static.ts
@@ -0,0 +1,76 @@
+export const introduction = [
+  {
+    id: "[01]",
+    title: "Empathy",
+    description:
+      "One of my strength is empathy for each client and users. I can better identify the client's pain points and understand the client's request easily.",
+  },
+  {
+    id: "[02]",
+    title: "Responsibility",
+    description:
+      "Due to self-organisation and self-discipline, I approach each project responsibly, completing my work step by step and informing about completed items on time.",
+  },
+  {
+    id: "[03]",
+    title: "Team-member",
+    description:
+      "One of the most important aspects of my work is teamwork with designers, marketing specialists and developers. Working together allows complex tasks to be solved faster and with higher quality. ",
+  },
+  {
+    id: "[04]",
+    title: "Soft skills",
+    description: [
+      "Systems thinking;",
+      "design thinking;",
+      "cross-team collaboration, creativity and adaptability.",
+    ],
+  },
+  {
+    id: "[05]",
+    title: "Hard skills",
+    description: [
+      "UX/UI design;",
+      "marketing tools (target audience and competitive analysis);",
+      "wireframing and prototyping;",
+      "3D animations, SEO;",
+      "no-code development.",
+    ],
+  },
+  {
+    id: "[06]",
+    title: "Languages",
+    description: [
+      "Russian (native);",
+      "English (full professional proficiency);",
+      "Finnish (professional working proficiency).",
+    ],
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "GreenCarLane",
+    description: "Web page creating",
+    image: "/greenCarLane.png",
+  },
+  {
+    id: 2,
+    title: "AppyBite App",
+    description: "Mobile App",
+    image: "/appyBite.png",
+  },
+  {
+    id: 3,
+    title: "Chameleo",
+    description: "Web App MVP",
+    image: "/chameleo.png",
+  },
+  {
+    id: 4,
+    title: "RoKate",
+    description: "Identity & Branding",
+    image: "/roKate.png",
+  },
+];

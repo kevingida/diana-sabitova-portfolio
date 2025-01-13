862b3ec315a2b0e736a70a43a8dde8fe79cc0f88
kevingida <kevingida@gmail.com>
2025-01-13 12:24:01 +0100

    add interface

---
+++ interface/Interface.tsx
@@ -0,0 +1,8 @@
+export interface svgProps extends React.SVGProps<SVGSVGElement> {}
+
+export interface bannerProps {
+  text: string;
+  textColour: string;
+  bgColour: string;
+  direction?: string;
+}

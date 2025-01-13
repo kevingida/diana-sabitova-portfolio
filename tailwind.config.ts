58de4dacf0a4166568b8a13bc2e3aa2f7afbffb6
kevingida <kevingida@gmail.com>
2025-01-13 12:22:17 +0100

    add animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,27 @@ export default {
       colors: {
         purple: "#BDC2F6",
         saladGreen: "#E3FD9A",
+        lightBlack: "#101010",
       },
       screens: {
         sm: "390px",
         md: "768px",
         lg: "1440px",
         xl: "1920px",
+      },
+      keyframes: {
+        "infinite-scroll-left": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        "infinite-scroll-right": {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        "infinite-scroll-left": "infinite-scroll-left 10s linear infinite",
+        "infinite-scroll-right": "infinite-scroll-right 10s linear infinite",
       },
     },
   },

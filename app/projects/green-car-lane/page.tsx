8b810948ac91a25bab0f0f62fafd6c1ab801a335
kevingida <kevingida@gmail.com>
2025-01-15 08:51:03 +0100

    responsive for mobile

--- app/projects/green-car-lane/page.tsx
+++ app/projects/green-car-lane/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const GreenCarLanePage = () => {
   return (
     <>
-      <div className=" max-w-screen-xl mx-auto bg-raisinBlack w-full flex flex-col items-center lg:px-[100px] xl:px-[120px]">
+      <div className=" max-w-screen-xl mx-auto bg-raisinBlack w-full flex flex-col items-center px-4 lg:px-[100px] xl:px-[120px]">
         <Cursor />
         <Image
           src="/greenCarLane-hero-desktop.png"
@@ -13,24 +13,26 @@ const GreenCarLanePage = () => {
           width={1680}
           height={994}
         />
-        <div className="flex flex-row justify-between items-center w-full mt-12 mb-[120px] h-[720px] ">
-          <div className=" lg:w-[770px] xl:w-[1041px] h-full bg-saladGreen rounded-[50px] lg:px-12 xl:px-[65px] lg:py-[60px] xl:py-[90px] text-darkPurple">
-            <p className=" text-[24px] font-medium leading-[52px] pb-[58px]">
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center w-full mt-6 mb-16 lg:mt-12 lg:mb-[120px] lg:h-[720px] gap-6">
+          <div className=" w-full h-[245px] lg:w-[770px] xl:w-[1041px] lg:h-full bg-saladGreen rounded-[16px] lg:rounded-[50px] px-6 py-7 lg:px-12 xl:px-[65px] lg:py-[60px] xl:py-[90px] text-darkPurple">
+            <p className=" text-[8px] lg:text-[24px] font-medium leading-[24px] lg:leading-[52px] pb-[19px] lg:pb-[58px]">
               A UX/UI Case Study
             </p>
-            <p className=" lg:text-[48px] xl:text-[60px] leading-[60px] pb-[230px]">
+            <p className=" text-[24px] lg:text-[48px] xl:text-[60px] leading-[24px] lg:leading-[60px] pb-[60px] lg:pb-[230px]">
               GreenCarLane Knowledge Hub Redesign
             </p>
-            <div className="grid grid-cols-[170px_auto] gap-y-4 text-[24px]">
+            <div className="grid grid-cols-[60px_auto] lg:grid-cols-[170px_auto] gap-y-4 text-[8px] lg:text-[24px]">
               <p>PERIOD</p>
               <p>2024</p>
               <p>ROLE</p>
               <p>Junior UX/UI Designer</p>
             </div>
           </div>
-          <div className=" lg:w-[450px] xl:w-[615px] h-full bg-purple rounded-[50px] lg:px-12 xl:px-[60px] lg:py-[60px] xl:py-[90px] text-darkPurple flex flex-col gap-8">
-            <p className=" text-[24px] leading-[36px]">Project Overview</p>
-            <p className=" text-[#331386] lg:text-[20px] xl:text-[24px] leading-[36px]">
+          <div className=" w-full h-fit lg:w-[450px] xl:w-[615px] lg:h-full bg-purple rounded-[16px] lg:rounded-[50px] px-6 py-7 lg:px-12 xl:px-[60px] lg:py-[60px] xl:py-[90px] text-darkPurple flex flex-col gap-4 lg:gap-8">
+            <p className=" text-[16px] lg:text-[24px] leading-[22px] lg:leading-[36px]">
+              Project Overview
+            </p>
+            <p className=" text-[#331386] text-[14px] lg:text-[20px] xl:text-[24px] leading-[24px] lg:leading-[36px]">
               As a junior UX/UI designer at GreenCarLane, I was tasked with
               redesigning the Press Release (Knowledge Hub) page to improve user
               experience and align with brand standards. The project focused on
@@ -40,11 +42,11 @@ const GreenCarLanePage = () => {
             </p>
           </div>
         </div>
-        <div className=" w-full flex flex-row justify-start items-start">
-          <p className="text-white text-[30px] leading-[60px] w-[625px]">
+        <div className=" w-full flex flex-col lg:flex-row justify-start items-start">
+          <p className="text-white text-[20px] lg:text-[30px] leading-[60px] w-[625px] pl-4 lg:pl-0">
             Problem Statement
           </p>
-          <ul className="list-disc text-[#BCBCBC] text-[30px] leading-[47px]">
+          <ul className="list-disc text-[#BCBCBC] text-[14px] lg:text-[30px] leading-[24px] lg:leading-[47px] pl-10 lg:pl-0">
             <li>Inconsistent visual design with the main website</li>
             <li>Poor content organization and hierarchy</li>
             <li>Lack of a standardized article template</li>
@@ -61,15 +63,15 @@ const GreenCarLanePage = () => {
           alt="greenCarLane previous design"
           width={1680}
           height={994}
-          className=" my-[90px]"
+          className=" my-16 lg:my-[90px]"
         />
         <div>
-          <div className="flex-grow border-t border-[#D9D9D9]" />
-          <div className=" w-full py-[80px] flex flex-row justify-start items-start">
-            <p className=" min-w-[547px] text-[30px] leading-[60px] text-white">
+          <div className="hidden lg:block flex-grow border-t border-[#D9D9D9]" />
+          <div className=" w-full pb-8 lg:py-[80px] flex flex-col lg:flex-row justify-start items-start">
+            <p className=" lg:min-w-[547px] text-[20px] lg:text-[30px] leading-[30px] lg:leading-[60px] text-white">
               Goals
             </p>
-            <ul className=" pl-8 list-decimal text-[#BCBCBC] text-[30px] leading-[47px]">
+            <ul className=" pl-8 list-decimal text-[#BCBCBC] text-[14px] lg:text-[30px] leading-[24px] lg:leading-[47px]">
               <li>
                 Create a unified design language aligned with
                 GreenCarLane&apos;s brand identity
@@ -85,15 +87,15 @@ const GreenCarLanePage = () => {
             </ul>
           </div>
           <div className="flex-grow border-t border-[#D9D9D9]" />
-          <div className=" w-full py-[80px] flex flex-row justify-start items-start">
-            <p className=" min-w-[547px] text-[30px] leading-[60px] text-white">
+          <div className=" w-full py-8 lg:py-[80px] flex flex-col lg:flex-row justify-start items-start">
+            <p className=" lg:min-w-[547px] text-[20px] lg:text-[30px] leading-[30px] lg:leading-[60px] text-white">
               Process
             </p>
-            <div className=" flex flex-col gap-[58px]">
-              <p className="text-white text-[48px] leading-[60px]">
+            <div className=" flex flex-col gap-4 lg:gap-[58px]">
+              <p className="text-white text-[24px] lg:text-[48px] leading-[60px]">
                 Research and Analysis
               </p>
-              <p className="text-[#BCBCBC] text-[30px] leading-[47px]">
+              <p className="text-[#BCBCBC] text-[14px] lg:text-[30px] leading-[24px] lg:leading-[47px]">
                 The design process began with a comprehensive analysis of the
                 existing Press Release page and the company&apos;s digital
                 ecosystem. Designers carefully examined the current
@@ -106,15 +108,15 @@ const GreenCarLanePage = () => {
             </div>
           </div>
           <div className="flex-grow border-t border-[#D9D9D9]" />
-          <div className=" w-full py-[80px] flex flex-row justify-start items-start">
-            <p className=" min-w-[547px] text-[30px] leading-[60px] text-white">
+          <div className=" w-full pt-8 lg:py-[80px] flex flex-col lg:flex-row justify-start items-start">
+            <p className=" lg:min-w-[547px] text-[20px] lg:text-[30px] leading-[30px] lg:leading-[60px] text-white">
               Process
             </p>
-            <div className=" flex flex-col gap-[58px]">
-              <p className="text-white text-[48px] leading-[60px]">
+            <div className=" flex flex-col gap-4 lg:gap-[58px]">
+              <p className="text-white text-[24px] lg:text-[48px] leading-[34px] lg:leading-[60px]">
                 Lo-Fi Prototype and Visual Design Development
               </p>
-              <p className="text-[#BCBCBC] text-[30px] leading-[47px]">
+              <p className="text-[#BCBCBC] text-[14px] lg:text-[30px] leading-[24px] lg:leading-[47px]">
                 This design phase is focused on creating a cohesive visual
                 language that aligns with GreenCarLane&apos;s brand identity.
                 They implemented a consistent color palette, typography, and
@@ -131,19 +133,19 @@ const GreenCarLanePage = () => {
           alt="greenCarLane process design"
           width={1680}
           height={2603}
-          className=" my-[90px]"
+          className=" my-16 lg:my-[90px]"
         />
         <div>
           <div className="flex-grow border-t border-[#D9D9D9]" />
-          <div className=" w-full py-[80px] flex flex-row justify-start items-start">
-            <p className=" min-w-[547px] text-[30px] leading-[60px] text-white">
+          <div className=" w-full py-8 lg:py-[80px] flex flex-col lg:flex-row justify-start items-start">
+            <p className=" lg:min-w-[547px] text-[20px] lg:text-[30px] leading-[30px] lg:leading-[60px] text-white">
               Process
             </p>
-            <div className=" flex flex-col gap-[58px]">
-              <p className="text-white text-[48px] leading-[60px]">
+            <div className=" flex flex-col gap-4 lg:gap-[58px]">
+              <p className="text-white text-[24px] lg:text-[48px] leading-[34px] lg:leading-[60px]">
                 Interaction and Prototype Creation
               </p>
-              <p className="text-[#BCBCBC] text-[30px] leading-[47px]">
+              <p className="text-[#BCBCBC] text-[14px] lg:text-[30px] leading-[24px] lg:leading-[47px]">
                 A detailed prototype was developed to test the new Knowledge Hub
                 design. This involved creating interactive mockups that
                 demonstrated the flow between different sections, article
@@ -154,15 +156,15 @@ const GreenCarLanePage = () => {
             </div>
           </div>
           <div className="flex-grow border-t border-[#D9D9D9]" />
-          <div className=" w-full py-[80px] flex flex-row justify-start items-start">
-            <p className=" min-w-[547px] text-[30px] leading-[60px] text-white">
+          <div className=" w-full pt-8 lg:py-[80px] flex flex-col lg:flex-row justify-start items-start">
+            <p className=" lg:min-w-[547px] text-[20px] lg:text-[30px] leading-[30px] lg:leading-[60px] text-white">
               Progress
             </p>
-            <div className=" flex flex-col gap-[58px]">
-              <p className="text-white text-[48px] leading-[60px]">
+            <div className=" flex flex-col gap-4 lg:gap-[58px]">
+              <p className="text-white text-[24px] lg:text-[48px] leading-[34px] lg:leading-[60px]">
                 Final Implementation
               </p>
-              <p className="text-[#BCBCBC] text-[30px] leading-[47px]">
+              <p className="text-[#BCBCBC] text-[14px] lg:text-[30px] leading-[24px] lg:leading-[47px]">
                 The final design was carefully prepared for development, with
                 detailed specifications for layout, interactions, and responsive
                 behavior. The team created comprehensive design guidelines to
@@ -177,17 +179,17 @@ const GreenCarLanePage = () => {
           alt="greenCarLane result design"
           width={1680}
           height={4371}
-          className=" my-[90px]"
+          className=" my-16 lg:my-[90px]"
         />
-        <div className=" w-full py-[80px] flex flex-row justify-start items-start">
-          <p className=" min-w-[547px] text-[30px] leading-[60px] text-white">
-            Progress
+        <div className=" w-full pb-[90px] lg:py-[80px] flex flex-col lg:flex-row justify-start items-start">
+          <p className=" lg:min-w-[547px] text-[20px] lg:text-[30px] leading-[30px] lg:leading-[60px] text-white">
+            Results and Impact
           </p>
           <div className=" flex flex-col gap-2">
-            <p className="text-[#BCBCBC] text-[30px] leading-[47px]">
+            <p className="text-[#BCBCBC] text-[14px] lg:text-[30px] leading-[24px] lg:leading-[47px]">
               The redesigned Knowledge Hub showcases significant improvements:
             </p>
-            <ul className=" pl-8 list-disc text-[#BCBCBC] text-[30px] leading-[47px]">
+            <ul className=" pl-8 list-disc text-[#BCBCBC] text-[14px] lg:text-[30px] leading-[24px] lg:leading-[47px]">
               <li>
                 <span className=" font-bold">Consistent Footer:</span> The
                 updated footer now includes a subscription field, aligning with
